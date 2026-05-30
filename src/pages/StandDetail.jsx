0f56164a1f1cfc2f3c25594903fbc35f9f1bee86
thebadmomgoodmom <thebadmomgoodmom@gmail.com>
2026-05-30 14:15:53 -0600

    Fix: remove leaflet CSS import from StandDetail

--- src/pages/StandDetail.jsx
+++ src/pages/StandDetail.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect, useCallback } from "react";
 import { MapContainer, TileLayer, Marker } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 
 delete L.Icon.Default.prototype._getIconUrl;
